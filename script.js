40d0553496e4cf0373f5c89f2f53f6967038b836
muhammadaqilulyaa-tech <muhammadaqilulyaa@gmail.com>
2026-05-12 23:46:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/* =========================
+   ACTIVE NAV LINK
+========================= */
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop - 140;
+    const sectionHeight = section.clientHeight;
+
+    if(scrollY >= sectionTop){
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if(link.getAttribute("href") === `#${current}`){
+      link.classList.add("active");
+    }
+
+  });
+
+});
